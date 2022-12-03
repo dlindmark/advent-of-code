20c41c037af87f699d7d9c8ede72ecdab5371a15
daniel lindmark <daniel.lindmark@protonmail.com>
2022-12-03 17:04:31 +0100

    day3

---
+++ Y2022/src/day3.rs
@@ -0,0 +1,64 @@
+use utils::{Answer, read_lines};
+
+pub fn solve(filename: String) -> Answer{
+    return Answer{challenge1: challenge1(&filename), challenge2: challenge2(&filename)};
+}
+
+fn get_priority(item: char) -> i64 {
+    let priority: i64;
+    if item.is_lowercase(){
+        priority = (item as u8 - 96) as i64;
+    }
+    else if item.is_uppercase() {
+        priority = (item as u8 - 38) as i64;
+    }
+    else{
+        priority = 0;
+    }
+    return priority;
+}
+
+fn challenge1(filename: &String) -> i64{
+    let mut a1: i64 = 0;
+
+    if let Ok(lines) = read_lines(filename) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(characters) = line {
+                // println!("{}", characters.chars().nth(0).unwrap());
+                // let pack1 = &characters[0..characters.len() / 2];
+                let pack2: &str = &characters[characters.len() / 2..];
+                for c in characters.chars() {
+                    if pack2.contains(c) {
+                        a1 += get_priority(c);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return a1
+}
+
+
+fn challenge2(filename: &String) -> i64{
+    let mut a1: i64 = 0;
+
+    if let Ok(lines) = read_lines(filename) {
+        let mut peekable_lines = lines.peekable();
+        while peekable_lines.peek().is_some(){
+            let line1 = peekable_lines.next().unwrap().unwrap();
+            let line2 = peekable_lines.next().unwrap().unwrap();
+            let line3 = peekable_lines.next().unwrap().unwrap();
+            for c in line1.chars() {
+                if line2.contains(c) {
+                    if line3.contains(c){
+                        a1 += get_priority(c);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return a1
+}
--- Y2022/src/main.rs
+++ Y2022/src/main.rs
@@ -2,6 +2,7 @@ use utils::Answer;
 
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     let a1: Answer = day1::solve("day1.txt".to_string());
@@ -9,6 +10,9 @@ fn main() {
 
     let a2: Answer = day2::solve("day2.txt".to_string());
     println!("Day2: {:?}", a2);
+
+    let a3: Answer = day3::solve("day3.txt".to_string());
+    println!("Day3: {:?}", a3);
 }
 
 #[cfg(test)]
@@ -26,5 +30,12 @@ mod tests {
         let a: Answer = day2::solve("day2.txt".to_string());
         assert_eq!(a.challenge1, 12740);
         assert_eq!(a.challenge2, 11980);
+    }
+
+    #[test]
+    fn day3() {
+        let a: Answer = day3::solve("day3.txt".to_string());
+        assert_eq!(a.challenge1, 8240);
+        assert_eq!(a.challenge2, 2587);
     }
 }
